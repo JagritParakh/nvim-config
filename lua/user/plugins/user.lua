aeeaa73568b6189a135b150cce5f1494e15e2ee9
Jagrit Parakh <jagritparakh@gmail.com>
2024-02-09 16:54:29 +0530

    Initial commit

---
+++ lua/user/plugins/user.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "github/copilot.vim",
+    event = "VeryLazy",
+    version = "*"
+  },
+  {
+  "catppuccin/nvim",
+  name = "catppuccin"
+  },
+  {
+    "pocco81/auto-save.nvim",
+    event = "VeryLazy",
+    version = "*"
+  },
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+}
